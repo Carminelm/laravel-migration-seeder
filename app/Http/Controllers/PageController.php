98448bb601d7b484860f57f3e24cd9b162b5adc4
Carminelm <carminelm54@gmail.com>
2024-09-12 15:34:05 +0200

    create migration

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,8 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Train;
 
 class PageController extends Controller
 {
-    //
+    public function index (){
+
+
+
+
+        return view('home',compact('trains'));
+    }
+
+
+    public function contacts (){
+        return view('contacts');
+    }
+
+
+    public function about (){
+        return view('about');
+    }
+
+
+    public function trains (){
+
+
+        $trains = Train::orderBy('id')->get();
+
+
+
+
+        return view('home',compact('trains'));
+    }
 }
