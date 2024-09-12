b845fd6ce2334f131b235b4694cc2d7ad5bce853
Carminelm <carminelm54@gmail.com>
2024-09-12 16:11:50 +0200

    add database

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -9,6 +9,7 @@ class PageController extends Controller
 {
     public function index (){
 
+        $trains = Train::orderBy('id')->get();
 
 
 
--- database/migrations/2024_09_12_133306_created_table.php
+++ database/migrations/2024_09_12_133306_created_table.php
@@ -11,7 +11,20 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('trains', function(Blueprint $table){
+            $table->id();
+            $table->string('azienda'); // Azienda
+            $table->string('stazione_di_partenza'); // Stazione di partenza
+            $table->string('stazione_di_arrivo'); // Stazione di arrivo
+            $table->time('orario_di_partenza'); // Orario di partenza
+            $table->time('orario_di_arrivo'); // Orario di arrivo
+            $table->string('codice_treno'); // Codice treno
+            $table->integer('numero_carrozze'); // Numero di carrozze
+            $table->boolean('in_orario')->default(true); // In orario
+            $table->boolean('cancellato')->default(false); // Cancellato
+            $table->timestamps();
+
+        });
     }
 
     /**
@@ -19,6 +32,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('trains');
     }
 };
