d5ccde2c7ce4f3f6f39f6defa96548470c62c846
Carminelm <carminelm54@gmail.com>
2024-09-14 15:56:24 +0200

    add Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace Database\Seeders;
+use App\Models\Train;
+use Faker\Generator as faker;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -10,13 +12,32 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run(faker $faker): void
     {
-        // \App\Models\User::factory(10)->create();
+        for($i = 0;$i < 10; $i++){
+            $train = new Train();
+            $train->azienda = $faker->randomElement(['Italo','Freccia Rossa','Intercity','Freccia Argento']);
+            $train->stazione_di_partenza = $faker->randomElement(['Roma Termini','Milano Centrale','Napoli Centrale','Torino Centrale']);
+            $train->stazione_di_arrivo = $faker->randomElement(['Roma Termini','Milano Centrale','Napoli Centrale','Torino Centrale']);
+            $train->orario_di_partenza = $faker->time(); // Orario di partenza casuale
+            $train->orario_di_arrivo = $faker->time(); // Orario di arrivo casuale
+            $train->codice_treno = 'TRN' . $faker->unique()->randomNumber(5); // Codice treno univoco
+            $train->numero_carrozze = $faker->numberBetween(5, 15); // Numero di carrozze
+            $train->in_orario = $faker->boolean(); // In orario o no
+            $train->cancellato = $faker->boolean(10); // 10% di probabilità che sia cancellato
+            $train->created_at = now();
+            $train->updated_at = now();
+    
+    
+            $train->save();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+
+
+        }
+
+
+
+
     }
 }
+
